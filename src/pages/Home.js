99eaca2b3be79a0c43f704ea7d419dff8b91caf5
Johan Ricardo <johanricardo@iMac-de-Christian.local>
2021-07-19 22:01:15 -0500

    Firts commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="d-flex">
+                    <div className="">grid</div>
+                    <div className="">grid</div>
+                    <div className="">grid</div>
+                </div>
+            </div>
+            
+            <p>lorem  ipsum dolor sit amet</p>
+
+            
+        </>
+    )
+}
+
+export default Home
